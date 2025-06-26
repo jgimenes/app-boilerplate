8e1cfe3d9ac1c79a2b06896dc4a3e59b463208ad
jgimenes <jgimenes@gmail.com>
2025-06-26 08:02:29 -0300

    feat(api): working with auth

--- apps/api/src/resources/admin/auth/auth.service.ts
+++ apps/api/src/resources/admin/auth/auth.service.ts
@@ -1,14 +1,24 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { otpUtils } from 'src/utils/otp.utils';
 import { SignInRequestDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger(AuthService.name);
+  private readonly unauthorizedErrorMessage =
+    'Email or password is not invalid';
+
   constructor(private readonly prisma: PrismaService) {}
   async signInAdminAccount(request: SignInRequestDto): Promise<void> {
     const { email, otp } = request;
     console.log(otp);
+
     // Find the admin account by email
 
     const adminAccount = await this.prisma.adminAccount.findUnique({
@@ -17,11 +27,32 @@ export class AuthService {
       },
     });
 
+    // If the admin account does not exist, throw an error
+
     if (!adminAccount) {
       this.logger.warn(`Admin account with email ${email} not found`);
-      throw new BadRequestException('Email or password is not invalid');
+      throw new UnauthorizedException(this.unauthorizedErrorMessage);
     }
 
-    // Verify the OTP
+    const otpHashed = adminAccount.otp;
+    const expiresOtp = adminAccount.otpExpiresAt;
+
+    // Check if the OTP is expired
+
+    if (expiresOtp && new Date() > expiresOtp) {
+      this.logger.warn(`OTP for admin account with email ${email} has expired`);
+      throw new BadRequestException(
+        'OTP has expired, please make a new login '
+      );
+    }
+
+    // Compare the provided OTP with the stored hashed OTP
+
+    if (otpHashed && !(await otpUtils.compareOTP(otp, otpHashed))) {
+      this.logger.warn(`Invalid OTP for admin account with email ${email}`);
+      throw new UnauthorizedException(this.unauthorizedErrorMessage);
+    }
   }
+
+  //TODO: Implemet jwt token generation and return it after successful login
 }
